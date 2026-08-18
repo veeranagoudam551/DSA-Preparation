8968c3bcee0f86aa9911392edebe35e05b8f5f4c
veeranagoudam551 <veeranagoudam551@gmail.com>
2026-08-18 12:17:19 +0530

    First and last occurrence solution

---
+++ Binary Search/First and last occurrence.py
@@ -0,0 +1,34 @@
+#Time Complexity:0(logn)
+#space Complexity:0(1)
+def first_last_occrurrence(arr,target):
+    first=-1
+    last=-1
+    #lower bound
+    low=0
+    high=len(arr)-1
+    while low<=high:
+        mid=(low+high)//2
+        if arr[mid]==target:
+            first=mid
+            high=mid-1
+        elif arr[mid]<target:
+            low=mid+1
+        else:
+            high=mid-1
+    #upper bound
+    low=0
+    high=len(arr)-1
+    while low<=high:
+        mid=(low+high)//2
+        if arr[mid]==target:
+            last=mid
+            low=mid+1
+        elif arr[mid]<target:
+            low=mid+1
+        else:
+            high=mid-1 
+    return [first,last]         
+
+target=int(input("Enter the target value:"))
+arr=[1, 2, 2, 2, 3, 4, 4, 5,6]
+print(first_last_occrurrence(arr,target))
