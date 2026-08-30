24db5c8f9e7670285562801cc362cffa113eda34
veeranagoudam551 <veeranagoudam551@gmail.com>
2026-08-30 11:08:05 +0530

    creating _list_forward_Backward

---
+++ doubly_linked_list/creating _list_forward_Backward.py
@@ -0,0 +1,52 @@
+class Node:
+    def __init__(self, data):
+        self.data = data
+        self.prev = None
+        self.next = None
+
+
+# Create nodes
+node1 = Node(10)
+node2 = Node(20)
+node3 = Node(30)
+node4 = Node(40)
+
+
+# Connect nodes
+node1.next = node2
+node2.prev = node1
+
+node2.next = node3
+node3.prev = node2
+
+node3.next = node4
+node4.prev = node3
+
+
+# Head and Tail
+head = node1
+tail = node4
+
+
+# Forward Traversal
+print("Forward Traversal:")
+
+current = head
+
+while current is not None:
+    print(current.data, end=" -> ")
+    current = current.next
+
+print("None")
+
+
+# Backward Traversal
+print("Backward Traversal:")
+
+current = tail
+
+while current is not None:
+    print(current.data, end=" -> ")
+    current = current.prev
+
+print("None")
